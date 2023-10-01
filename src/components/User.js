a5e3d4d1b14c286f006ba0227e04583a2d76148e
Ivan Franco <ivan.franco@derivco.co.uk>
2023-10-01 22:57:53 +0200

    Making use of component class for users

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import classes from './User.module.css';
 
 class User extends Component{
   render(){
-    return <li className={classes.user}>{props.name}</li>;
+    return <li className={classes.user}>{this.props.name}</li>;
   }
 };
 
