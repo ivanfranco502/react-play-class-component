b68215ad3da4b42eaff4edcb962a0fa4d974798b
Ivan Franco <ivan.franco@derivco.co.uk>
2023-10-01 21:13:11 +0200

    moving User to class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,10 @@
+import React, { Component } from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
+class User extends Component{
+  render(){
+    return <li className={classes.user}>{props.name}</li>;
+  }
 };
 
 export default User;
